efa245369f7beb3a7517e1f273b3cee0f45535cb
EvitanRelta <shauntanzongzhi@gmail.com>
2022-01-09 22:32:50 +0800

    Move controllers to /api path

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :tags
-  resources :categories
-  resources :tasks
+  namespace :api do
+    resources :tags
+    resources :categories
+    resources :tasks
+  end
+
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
