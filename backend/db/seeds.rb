ba230143d5feecc1f4ffe7dbce88bf68774093e3
EvitanRelta <shauntanzongzhi@gmail.com>
2022-01-09 23:40:17 +0800

    Add Rails seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(email: 'admin@admin.com', password: '123456', password_confirmation: '123456')
+Task.create(user_id: user.id, title: 'Task 1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+
+category = Category.create(user_id: user.id, name: 'Category 1', allow_multiple_tags: 'true')
+Tag.create(user_id: user.id, category_id: category.id, name: 'Tag 1')
