ab44c384021031569500ac542bc69e9769260032
EvitanRelta <shauntanzongzhi@gmail.com>
2022-01-09 19:37:17 +0800

    Update Rails models

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :tasks
+  has_many :categories
+  has_many :tags
 end
