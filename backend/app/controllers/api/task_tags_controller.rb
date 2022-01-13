6699b414b8a051f57cf6a1c5f214f6b4048156ed
EvitanRelta <shauntanzongzhi@gmail.com>
2022-01-13 17:33:42 +0800

    Add ability to add/remove Tags from Task

---
+++ backend/app/controllers/api/task_tags_controller.rb
@@ -0,0 +1,49 @@
+class Api::TaskTagsController < ApplicationController
+    before_action :set_api_task, only: [:create, :destroy]
+    before_action :authenticate_user!
+
+    # POST /api/:task/tags
+    def create
+      if not @task.tags.exists?(id: @tag.id)
+        if @task.tags << @tag
+          head :created
+        else
+          render json: @task.tags.errors, status: :unprocessable_entity
+        end
+      else
+        render json: {
+          tag: ["has already been added"]
+        }, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /api/:task/tags/1
+    def destroy
+      if @task.tags.exists?(id: @tag.id)
+        if @task.tags.delete(@tag)
+          head :ok
+        else
+          render json: @task.tags.errors, status: :unprocessable_entity
+        end
+      else
+        render json: {
+          tag: ["task does not have tag"]
+        }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_api_task
+        tag = Api::Tag.find_by(id: params[:id])
+        task = current_user.tasks.find_by(id: params[:task_id])
+      
+        if tag and current_user.categories.exists?(id: tag.category_id) and task
+          @tag = tag
+          @task = task
+        else
+          head :unauthorized
+        end
+      end
+  end
+  
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
     resources :tags, only: [:show, :create, :update, :destroy]
   end
   namespace :api do
-    resources :tasks
+    resources :tasks do
+      resources :tags, controller: :task_tags, only: [:destroy] do
+        member do
+          post '/' => 'task_tags#create'
+        end
+      end
+    end
   end
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
