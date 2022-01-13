21dede36740eae6bc0fc418a2b49d56342f9de52
EvitanRelta <shauntanzongzhi@gmail.com>
2022-01-13 16:22:10 +0800

    Update Rails Tag model

--- backend/app/models/api/tag.rb
+++ backend/app/models/api/tag.rb
@@ -2,6 +2,7 @@ class Api::Tag < ApplicationRecord
     belongs_to :category,
         class_name: "Api::Category",
         foreign_key: "category_id"
+    validates :name, uniqueness: { scope: :category }
 
     has_and_belongs_to_many :tasks, 
         class_name: "Api::Task",
