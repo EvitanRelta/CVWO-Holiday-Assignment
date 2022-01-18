805bd115e0112d905b626f751fa903e5267258e4
EvitanRelta <shauntanzongzhi@gmail.com>
2022-01-18 22:10:58 +0800

    Set default isDarKMode to use system pref.

--- frontend/src/store/reducers/isDarkMode.ts
+++ frontend/src/store/reducers/isDarkMode.ts
@@ -1,7 +1,13 @@
 import { Action, Reducer } from 'redux';
 import { SetDarkModeAction } from '../actionsCreators/setDarkMode';
 
-const isDarkMode: Reducer<boolean, SetDarkModeAction | Action<'isDarkMode/toggle'>> = (state = false, action) => {
+// Defaults to Light-Mode if fail to get preference.
+const systemPrefersDarkMode = (
+    window.matchMedia 
+        && window.matchMedia('(prefers-color-scheme: dark)').matches
+) || false;
+
+const isDarkMode: Reducer<boolean, SetDarkModeAction | Action<'isDarkMode/toggle'>> = (state = systemPrefersDarkMode, action) => {
     switch (action.type) {
         case 'isDarkMode/set':
             return action.payload;
