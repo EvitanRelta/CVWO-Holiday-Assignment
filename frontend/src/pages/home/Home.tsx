afdc05069eb5603d291656c577a02e42cf9b6513
EvitanRelta <shauntanzongzhi@gmail.com>
2022-01-20 10:44:44 +0800

    Update store imports in Home page

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -12,9 +12,9 @@ import { DarkMode, Google } from '@mui/icons-material';
 import { ResponsiveAppBar } from '../components';
 import {  } from '../components'
 
-import { RootState } from '../../store/reducers';
+import { RootState } from '../../store/combinedReducer';
 import { useDispatch, useSelector } from 'react-redux';
-import { toggleDarkMode } from '../../store/actionsCreators';
+import { toggleDarkMode } from '../../store/isDarkMode/actionCreators';
 
 interface HomeProps {}
 
