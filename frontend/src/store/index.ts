4a06ba11156a816b3da67dcb5d69da04a5082bf3
EvitanRelta <shauntanzongzhi@gmail.com>
2022-01-25 19:05:14 +0800

    Remove redux-devtools code

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -3,9 +3,6 @@ import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
 
-//@ts-ignore
-// https://github.com/zalmoxisus/redux-devtools-extension
-const composeWithDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
